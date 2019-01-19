3bf498534089ee42a61ac1799ea06934a24961ed
JayRodrig <mrjosemlrod@gmail.com>
2019-01-19 10:38:46 -0500

    added defineUsers func

--- DB/database.js
+++ DB/database.js
@@ -49,8 +49,6 @@ const createUser = (firstName, lastName, email, imgURL, story, resumeStr, resume
     });
 }
 
-// createUser('Jose', 'Rodriguez', 'joserodriguez@pursuit.org', '[insert imgURL]', '[insert story]', '[insert resumeStr]', '[insert resumeURL]', ['project1', 'project2']);
-
 /*
     @func getUsers
     @param  {str || ''} uid
@@ -75,12 +73,42 @@ const getUsers = uid => {
     })
     .then(data => {
         console.log(data);
-        console.log('Successfully retrieved data');
+        console.log(`
+        ---------------------------
+        Successfully retrieved data
+        `);
     })
     .catch(e => {
         console.log('Something went wrong', e.toString());
     });
 }
 
-// readUser('-LWXCSZaFd3qaceV4YIl');
-getUsers();
+/*
+    @func defineUsers
+    @param {undefined}
+    @desc - takes no arguments, logs an obj witht he list of id's as keys 
+            and the full name of the user of the corresponding id as value.
+*/
+const defineUsers = () => {
+    // this is the Object that will hold the 'ID's' as keys.
+    const fellowsIDs = {};
+
+    return firebase.database().ref('/cohort5_0/class5_2/fellows/').once('value').then(function(snapshot) {
+        return snapshot.val();
+    })
+    .then(data => {
+        // id retreived from the array of the keys of the data Object
+        const id = Object.keys(data);
+        id.forEach(ele => {
+        // full name retrieved by concatenating first name and last name;
+         const userWithId = `${data[ele].firstName} ${data[ele].lastName}`;
+         fellowsIDs[ele] = userWithId;
+        })
+    
+        console.log('Ids and UserNames Retrieved: \n', fellowsIDs);
+        
+    })
+    .catch(e => {
+        console.log('Something went wrong', e.toString());
+    });
+}
